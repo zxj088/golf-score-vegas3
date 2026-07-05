24ba850b5b067dacce177f84568f0965b2d09f8a
zxj088 <zxj100@gmail.com>
2026-07-05 20:07:58 +0200

    Connect Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.VEGAS_SUPABASE = {
-  url: '',
-  anonKey: '',
+  url: 'https://ophifvihhnaqrroneqbhb.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9waWZ2aWhobmFxcnJvbmVxYmhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyNjQxNTgsImV4cCI6MjA5ODg0MDE1OH0.eFNZ1OqyAN__JmYlT3OXQS306o0I27ONaGCnC5c9SiE',
   syncKey: 'default'
 };
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vegas-golf-v8';
+const CACHE_NAME = 'vegas-golf-v9';
 const ASSETS = [
   './',
   './index.html',
